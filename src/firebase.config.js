3e92da0bed0e4c5d131bb7c7f476c656347397c8
Luis Gonzalez <lmgl887@gmail.com>
2023-01-18 00:17:01 -0600

    add empty project

---
+++ src/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBjZvBZ_ZVUdj6hFPAO6w602tqiM-M3zck",
+  authDomain: "realstate-marketplace-app.firebaseapp.com",
+  projectId: "realstate-marketplace-app",
+  storageBucket: "realstate-marketplace-app.appspot.com",
+  messagingSenderId: "380161465561",
+  appId: "1:380161465561:web:c6fcdf408ca362d07272c8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
