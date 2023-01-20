eea226c8d6fc8cac4dd880e8198d86ae14dd69f5
Luis Gonzalez <lmgl887@gmail.com>
2023-01-20 00:22:53 -0600

    display username after sign in

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,15 @@
+import { getAuth } from "firebase/auth"
+import { useEffect, useState } from "react"
+
 function Profile() {
-  return (
-    <>
-      <h1>Profile</h1>
-    </>
-  )
+  const auth = getAuth()
+  const [user, setUser] = useState(null)
+
+  useEffect(()=>{
+    setUser(auth.currentUser)
+  },[])
+
+  return user ? <h1>{user.displayName}</h1> : 'Not Logged'
 }
 
 export default Profile
