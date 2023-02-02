8179e9d0d468c85d4a663d40773fd2db393faf5d
Luis Gonzalez <lmgl887@gmail.com>
2023-02-02 01:07:05 -0600

    add listing details screen

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -89,7 +89,7 @@ function CreateListing() {
     if (isMounted) {
       onAuthStateChanged(auth, (user) => {
         if (user) {
-          setFormData({ ...formData, useRef: user.uid })
+          setFormData({ ...formData, userRef: user.uid })
         } else {
           navigate('/sign-in')
         }
--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -5,7 +5,7 @@ import { getAuth } from 'firebase/auth'
 import { db } from '../firebase.config'
 import Spinner from "../components/Spinner"
 import shareIcon from '../assets/svg/shareIcon.svg'
-import { async } from "@firebase/util"
+import { toast } from "react-toastify"
 
 
 
@@ -35,9 +35,63 @@ function Listing() {
 
   }, [navigate, params.listingId])
 
-
+  if (loading) {
+    return <Spinner />
+  }
   return (
-    <div>Listing</div>
+    <main>
+
+      <div className="shareIconDiv" onClick={() => {
+        navigator.clipboard.writeText(window.location.href)
+        setIsShareLinkCopied(true)
+        setTimeout(() => {
+          setIsShareLinkCopied(false)
+        }, 2000)
+      }}>
+        <img src={shareIcon} alt="" />
+      </div>
+
+      {isShareLinkCopied && <p className="linkCopied">Link Copied</p>}
+      <div className="listingDetails">
+        <p className="listingName">
+          {listing.name} - ${Listing.isOffer
+            ? listing.discountedPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+            : listing.regularPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+        </p>
+        <p className="listingLocation">{`${listing.location}, ${listing.country}`}</p>
+        <p className="listingType">For {listing.type === 'rent' ? 'Rent' : 'Sale'}</p>
+        {listing.isOffer && (
+          <p className="discountPrice">
+            ${listing.regularPrice - listing.discountedPrice} discount
+          </p>
+        )}
+
+        <ul className="listingDetailsList">
+          <li>
+            {
+            listing.bedrooms > 1 
+            ? `${listing.bedrooms} Bedrooms` 
+            : `1 Bedroom`
+            }
+          </li>
+          <li>
+            {
+            listing.bathrooms > 1 
+                ? `${listing.bathrooms} Bathrooms` 
+                : `1 Bathroom`
+            }
+          </li>
+          <li>{listing.parking && 'Parking Spot'}</li>
+          <li>{listing.furnished && 'Furnished'}</li>
+        </ul>
+        <p className="listingLocationTitle">Location</p>
+
+        {auth.currentUser?.uid !== listing.userRef && (
+          <Link 
+          to={`/contact/${listing.userRef}?name=${listing.name}&location=${listing.address}`} className='primaryButton'>Contact Landlord</Link>
+        )}
+      </div>
+    </main>
   )
 }
 
